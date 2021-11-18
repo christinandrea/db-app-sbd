58dab44d69f280afe37665027f400715a7d3b8c8
Christina Andrea <christina.andrea@ti.ukdw.ac.id>
2021-11-18 12:14:59 +0700

    add conn to db

---
+++ conn.php
@@ -0,0 +1,14 @@
+<?php 
+
+    $host = 'localhost';
+    $username = 'root';
+    $pw = '';
+    $dbname = 'sekolah';
+
+    $conn = mysqli_connect($host,$username,$pw,$dbname);
+
+    if(mysqli_connect_errno()){
+        echo "Connection failed:". mysqli_connect_error();
+
+    }
+?>
